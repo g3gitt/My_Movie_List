4f12f9cb0eb4f91c63ae630c061aaaa2d2008d16
Gayathri Subramaniam <109906263+g3gitt@users.noreply.github.com>
2024-08-28 22:49:35 +0530

    Update MoviesGenerator.cpp

--- MoviesGenerator/MoviesGenerator.cpp
+++ MoviesGenerator/MoviesGenerator.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main()
 {
+	while (true) {
 	std::vector<std::string>movies;
 	std::ifstream MoviesFile("movies.txt");
 	for (std::string line; std::getline(MoviesFile, line);) {
@@ -17,10 +18,22 @@ int main()
 		movies.push_back(line);
 	}
 
-std::mt19937 rng(std::random_device{}());
-std::uniform_int_distribution<int>dist(0, movies.size() - 1);
-const std::string target = movies[dist(rng)];
+	std::mt19937 rng(std::random_device{}());
+	std::uniform_int_distribution<int>dist(0, movies.size() - 1);
+	const std::string target = movies[dist(rng)];
 
-cout << target;
-	return 0;
+	cout << "How about this movie?Text me 'cool' if ok or 'no'" << endl;
+	cout << target<<endl;
+
+	string inp;
+	cin >> inp;
+	if (inp == "cool") {
+		break;
+	}
+	else {
+		continue;
+	}
+}
+std::cin.get();
+return 0;
 }
